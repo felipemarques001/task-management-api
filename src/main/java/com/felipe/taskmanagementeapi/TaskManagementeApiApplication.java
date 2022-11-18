6d646852812c5230c4901f7a5c30353f656e3f5d
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-18 13:57:21 -0300

    Starting the project

---
+++ src/main/java/com/felipe/taskmanagementeapi/TaskManagementeApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.taskmanagementeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/taskmanagementeapi/TaskManagementeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.taskmanagementeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
