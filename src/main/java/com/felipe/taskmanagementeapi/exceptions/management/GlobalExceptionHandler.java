eeae90ddd9f3bbb6dd78c9103fcc93e7825317e3
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-20 14:57:15 -0300

    Creating the ErrorDetails and GlobalExceptionHandler classes to exceptions management

---
+++ src/main/java/com/felipe/taskmanagementeapi/exceptions/management/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.felipe.taskmanagementeapi.exceptions.management;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+}
