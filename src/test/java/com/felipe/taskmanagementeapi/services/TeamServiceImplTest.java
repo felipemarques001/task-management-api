1c33ad0f6630627c3057175c8df588aa0e9cd3e7
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-20 17:56:08 -0300

    Creating test to tests findTeamById method (success and fail cases) of the TeamServiceImpl class

--- src/test/java/com/felipe/taskmanagementeapi/services/TeamServiceImplTest.java
+++ src/test/java/com/felipe/taskmanagementeapi/services/TeamServiceImplTest.java
@@ -3,6 +3,7 @@ package com.felipe.taskmanagementeapi.services;
 import com.felipe.taskmanagementeapi.Repositories.TeamRepository;
 import com.felipe.taskmanagementeapi.dtos.TeamDto;
 import com.felipe.taskmanagementeapi.entities.TeamEntity;
+import com.felipe.taskmanagementeapi.exceptions.ResourceNotFoundException;
 import com.felipe.taskmanagementeapi.services.impl.TeamServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.BeanUtils;
 
 import java.util.HashSet;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -59,5 +61,37 @@ public class TeamServiceImplTest {
         assertEquals(NAME_TEAM, response.getName());
         assertEquals(0, response.getEmployees().size());
         assertEquals(0, response.getTasks().size());
+    }
+
+    @DisplayName("Unit test for findTeamById method - success case")
+    @Test
+    void givenValidId_whenFindTeamById_thenReturnSavedTeam() {
+        // given
+        Mockito.when(teamRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(savedTeamEntity));
+
+        // when
+        TeamDto response = teamService.findTeamById(ID_TEAM);
+
+        // then
+        assertEquals(ID_TEAM, response.getId());
+        assertEquals(NAME_TEAM, response.getName());
+        assertEquals(0, response.getEmployees().size());
+        assertEquals(0, response.getTasks().size());
+    }
+
+    @DisplayName("Unit test for findTeamById method - fail case")
+    @Test
+    void givenInvalidId_whenFindTeamById_thenThrowsResourceNotFoundException() {
+        // given
+        Mockito.when(teamRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+
+        // when
+        try {
+            TeamDto response = teamService.findTeamById(ID_TEAM);
+        } catch (Exception ex) {
+            // then
+            assertEquals(ResourceNotFoundException.class, ex.getClass());
+            assertEquals("Team not found with id : 1", ex.getMessage());
+        }
     }
 }
