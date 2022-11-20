23145adde29a6cf1dd24e199de7f2359bb3c3cc9
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-20 15:10:23 -0300

    In GlobalExceptionHandler class, I created a method to handle the ResourceNotFoundException

--- src/main/java/com/felipe/taskmanagementeapi/exceptions/management/GlobalExceptionHandler.java
+++ src/main/java/com/felipe/taskmanagementeapi/exceptions/management/GlobalExceptionHandler.java
@@ -1,8 +1,24 @@
 package com.felipe.taskmanagementeapi.exceptions.management;
 
+import com.felipe.taskmanagementeapi.exceptions.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception,
+                                                                        WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
+    }
 }
