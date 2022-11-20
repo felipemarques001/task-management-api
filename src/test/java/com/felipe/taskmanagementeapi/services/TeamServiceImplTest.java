f673827e0ca80c787abd2305237ab7596e6965dc
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-20 17:49:01 -0300

    Creating test to tests createTeam method of the TeamServiceImpl class
    I also created ID_TEAM field in TeamServiceImplTest and refactored the name of teamEntity field to savedTeamEntity

--- src/test/java/com/felipe/taskmanagementeapi/services/TeamServiceImplTest.java
+++ src/test/java/com/felipe/taskmanagementeapi/services/TeamServiceImplTest.java
@@ -5,16 +5,22 @@ import com.felipe.taskmanagementeapi.dtos.TeamDto;
 import com.felipe.taskmanagementeapi.entities.TeamEntity;
 import com.felipe.taskmanagementeapi.services.impl.TeamServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.BeanUtils;
 
 import java.util.HashSet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TeamServiceImplTest {
 
     private static final String NAME_TEAM = "Departamento de TI";
+    private static final Integer ID_TEAM = 1;
     private static final String NAME_TEAM2 = "Departamento de análise financeira";
 
     @InjectMocks
@@ -23,18 +29,35 @@ public class TeamServiceImplTest {
     @Mock
     private TeamRepository teamRepository;
 
-    private TeamEntity teamEntity;
+    private TeamEntity savedTeamEntity;
     private TeamDto teamDto;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        teamEntity = new TeamEntity();
-        teamEntity.setName(NAME_TEAM);
-        teamEntity.setEmployees(new HashSet<>());
-        teamEntity.setTasks(new HashSet<>());
+        savedTeamEntity = new TeamEntity();
+        savedTeamEntity.setId(ID_TEAM);
+        savedTeamEntity.setName(NAME_TEAM);
+        savedTeamEntity.setEmployees(new HashSet<>());
+        savedTeamEntity.setTasks(new HashSet<>());
 
         teamDto = new TeamDto();
-        BeanUtils.copyProperties(teamEntity, teamDto);
+        BeanUtils.copyProperties(savedTeamEntity, teamDto);
+    }
+
+    @DisplayName("Unit test for createTeam method")
+    @Test
+    void givenTeamDto_whenCreateTeam_thenReturnSavedTeam() {
+        // given
+        Mockito.when(teamRepository.save(Mockito.any(TeamEntity.class))).thenReturn(savedTeamEntity);
+
+        // when
+        TeamDto response = teamService.createTeam(teamDto);
+
+        // then
+        assertEquals(ID_TEAM, response.getId());
+        assertEquals(NAME_TEAM, response.getName());
+        assertEquals(0, response.getEmployees().size());
+        assertEquals(0, response.getTasks().size());
     }
 }
