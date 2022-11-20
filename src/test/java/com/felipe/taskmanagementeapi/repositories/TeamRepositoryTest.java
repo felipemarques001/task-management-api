2439115cf190d69771ce3c1ac7f32d3cf0eabe60
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-20 16:42:05 -0300

    Creating test to tests save method of the TeamRepository interface
    In teamEntity field, I also set the employees and tasks fields as a empty list

--- src/test/java/com/felipe/taskmanagementeapi/repositories/TeamRepositoryTest.java
+++ src/test/java/com/felipe/taskmanagementeapi/repositories/TeamRepositoryTest.java
@@ -3,8 +3,14 @@ package com.felipe.taskmanagementeapi.repositories;
 import com.felipe.taskmanagementeapi.Repositories.TeamRepository;
 import com.felipe.taskmanagementeapi.entities.TeamEntity;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class TeamRepositoryTest {
@@ -21,5 +27,25 @@ public class TeamRepositoryTest {
     void setUp() {
         teamEntity = new TeamEntity();
         teamEntity.setName(NAME_TEAM);
+        teamEntity.setEmployees(new HashSet<>());
+        teamEntity.setTasks(new HashSet<>());
     }
+
+    @DisplayName("Unit test for save method")
+    @Test
+    public void givenTeamEntity_whenSave_thenReturnSavedTeam() {
+        // given
+        // code in setUp method
+
+        //when
+        TeamEntity savedTeam = teamRepository.save(teamEntity);
+
+        // then
+        assertNotNull(savedTeam.getId());
+        assertEquals(NAME_TEAM, savedTeam.getName());
+        assertEquals(0, savedTeam.getEmployees().size());
+        assertEquals(0, savedTeam.getTasks().size());
+    }
+
+
 }
