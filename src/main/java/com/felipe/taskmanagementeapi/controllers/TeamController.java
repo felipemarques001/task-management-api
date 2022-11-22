c2f7aed2eb0dbabccb1cfc40acebeb4d1c622c66
felipemarques001 <felipemarquesgg@outlook.com>
2022-11-22 19:32:49 -0300

    changing the access modifiers of all methods of the TeamController class. I changed from private to public

--- src/main/java/com/felipe/taskmanagementeapi/controllers/TeamController.java
+++ src/main/java/com/felipe/taskmanagementeapi/controllers/TeamController.java
@@ -18,31 +18,31 @@ public class TeamController {
     private TeamServiceImpl teamService;
 
     @PostMapping
-    private ResponseEntity<TeamDto> createTeam(@RequestBody @Valid TeamDto teamDto){
+    public ResponseEntity<TeamDto> createTeam(@RequestBody @Valid TeamDto teamDto){
         TeamDto savedTeamDto = teamService.createTeam(teamDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedTeamDto);
     }
 
     @GetMapping(value = "/{id}")
-    private ResponseEntity<TeamDto> getTeamById(@PathVariable Integer id) {
+    public ResponseEntity<TeamDto> getTeamById(@PathVariable Integer id) {
         TeamDto foundedTeamDto = teamService.findTeamById(id);
         return ResponseEntity.status(HttpStatus.OK).body(foundedTeamDto);
     }
 
     @GetMapping
-    private ResponseEntity<List<TeamDto>> findAllTeams() {
+    public ResponseEntity<List<TeamDto>> findAllTeams() {
         List<TeamDto> teamDtoList = teamService.findAllTeams();
         return ResponseEntity.status(HttpStatus.OK).body(teamDtoList);
     }
 
     @PutMapping(value = "/{id}")
-    private ResponseEntity<TeamDto> updateNameTeam(@RequestBody @Valid TeamDto teamDto, @PathVariable Integer id) {
+    public ResponseEntity<TeamDto> updateNameTeam(@RequestBody @Valid TeamDto teamDto, @PathVariable Integer id) {
         TeamDto updatedTeamDto = teamService.updateTeamName(teamDto, id);
         return ResponseEntity.status(HttpStatus.OK).body(updatedTeamDto);
     }
 
     @DeleteMapping(value = "/{id}")
-    private ResponseEntity<String> deleteTeamById(@PathVariable Integer id) {
+    public ResponseEntity<String> deleteTeamById(@PathVariable Integer id) {
         String responseService = teamService.deleteTeamById(id);
         return ResponseEntity.status(HttpStatus.OK).body(responseService);
     }
